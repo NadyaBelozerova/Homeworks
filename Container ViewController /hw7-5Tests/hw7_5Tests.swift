90a57c278f54c1787ccb367a2addea34f05f2e46
NadyaBelozerova <71548324+NadyaBelozerova@users.noreply.github.com>
2020-10-20 17:22:02 +0500

    Container ViewController и UIPageViewController

---
+++ Container ViewController /hw7-5Tests/hw7_5Tests.swift
@@ -0,0 +1,34 @@
+//
+//  hw7_5Tests.swift
+//  hw7-5Tests
+//
+//  Created by Nadya Belozerova on 12.10.2020.
+//  Copyright © 2020 Nadya Belozerova. All rights reserved.
+//
+
+import XCTest
+@testable import hw7_5
+
+class hw7_5Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
