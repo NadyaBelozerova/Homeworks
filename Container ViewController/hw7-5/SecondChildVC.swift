b705899ffd8bf52cb8f72eff6b1c701df7a28a70
NadyaBelozerova <71548324+NadyaBelozerova@users.noreply.github.com>
2020-11-10 14:55:09 +0500

    ContainerViewController (10.11)

---
+++ Container ViewController/hw7-5/SecondChildVC.swift
@@ -0,0 +1,19 @@
+//
+//  SecondChildVC.swift
+//  hw7-5
+//
+//  Created by Nadya Belozerova on 29.10.2020.
+//  Copyright © 2020 Nadya Belozerova. All rights reserved.
+//
+
+import UIKit
+
+class SecondChildVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+       
+    }
+
+}
---
+++ Container ViewController/hw7-5/ThirdChildVC.swift
@@ -0,0 +1,19 @@
+//
+//  ThirdChildVC.swift
+//  hw7-5
+//
+//  Created by Nadya Belozerova on 29.10.2020.
+//  Copyright © 2020 Nadya Belozerova. All rights reserved.
+//
+
+import UIKit
+
+class ThirdChildVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+      
+    }
+
+}
