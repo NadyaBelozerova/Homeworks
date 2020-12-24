33d777f9125259abee42d4f91579a0d6043e0ad5
NadyaBelozerova <71548324+NadyaBelozerova@users.noreply.github.com>
2020-12-24 23:58:46 +0500

    Домашнее задание 10 (доработка)

--- Homework10/дз10-1/ViewController.swift
+++ Homework10/дз10-1/ViewController.swift
@@ -13,42 +13,47 @@ struct Setting {
     let label: String
     let isSwitchHidden: Bool
     let accessoryType1: UITableViewCell.AccessoryType
+    
+    init(name: String, icon: UIImage?, label: String = "", isSwitchHidden: Bool = true, accessoryType1: UITableViewCell.AccessoryType = .disclosureIndicator) {
+        self.name = name
+        self.icon = icon
+        self.label = label
+        self.isSwitchHidden = isSwitchHidden
+        self.accessoryType1 = accessoryType1
+        }
  
 }
 
 class ViewController: UIViewController {
     
-    var settings = [
-        Setting(name: "Авиарежим", icon: #imageLiteral(resourceName: "planemode"), label: "", isSwitchHidden: false, accessoryType1: .none),
-        Setting(name: "Wi-Fi", icon: #imageLiteral(resourceName: "wifi"), label: "Anvics-YOTA", isSwitchHidden: true, accessoryType1: .disclosureIndicator),
-        Setting(name: "Bluetooth", icon: #imageLiteral(resourceName: "bluetooth"), label: "Вкл.", isSwitchHidden: true, accessoryType1: .disclosureIndicator),
-        Setting(name: "Сотовая связь", icon: #imageLiteral(resourceName: "mobilenet"), label: "", isSwitchHidden: true, accessoryType1: .disclosureIndicator),
-        Setting(name: "Режим модема", icon: #imageLiteral(resourceName: "modemmode"), label: "", isSwitchHidden: true, accessoryType1: .disclosureIndicator),
-        Setting(name: "", icon: nil, label: "", isSwitchHidden: true, accessoryType1: .none),
-        Setting(name: "Уведомления", icon: #imageLiteral(resourceName: "notes"), label: "", isSwitchHidden: true, accessoryType1: .disclosureIndicator),
-        Setting(name: "Звуки, тактильные сигналы", icon: #imageLiteral(resourceName: "sounds"), label: "", isSwitchHidden: true, accessoryType1: .disclosureIndicator),
-        Setting(name: "Не беспокоить", icon: #imageLiteral(resourceName: "dontdist"), label: "", isSwitchHidden: true, accessoryType1: .disclosureIndicator),
-        Setting(name: "Экранное время", icon: #imageLiteral(resourceName: "screentime"), label: "", isSwitchHidden: true, accessoryType1: .disclosureIndicator),
-        Setting(name: "", icon: nil, label: "", isSwitchHidden: true, accessoryType1: .none),
-        Setting(name: "Основные", icon: #imageLiteral(resourceName: "main"), label: "", isSwitchHidden: true, accessoryType1: .disclosureIndicator),
-        Setting(name: "Пункт управления", icon: #imageLiteral(resourceName: "commandcenter"), label: "", isSwitchHidden: true, accessoryType1: .disclosureIndicator),
-        Setting(name: "Экран и яркость", icon: #imageLiteral(resourceName: "screen"), label: "", isSwitchHidden: true, accessoryType1: .disclosureIndicator)
+    var settings: [[Setting]] = [[Setting(name: "Авиарежим", icon: #imageLiteral(resourceName: "planemode"),  isSwitchHidden: false, accessoryType1: .none),
+        Setting(name: "Wi-Fi", icon: #imageLiteral(resourceName: "wifi"), label: "Anvics-YOTA"),
+        Setting(name: "Bluetooth", icon: #imageLiteral(resourceName: "bluetooth"), label: "Вкл."),
+        Setting(name: "Сотовая связь", icon: #imageLiteral(resourceName: "mobilenet")),
+        Setting(name: "Режим модема", icon: #imageLiteral(resourceName: "modemmode")),],
+        [Setting(name: "Уведомления", icon: #imageLiteral(resourceName: "notes")),
+        Setting(name: "Звуки, тактильные сигналы", icon: #imageLiteral(resourceName: "sounds")),
+        Setting(name: "Не беспокоить", icon: #imageLiteral(resourceName: "dontdist")),
+        Setting(name: "Экранное время", icon: #imageLiteral(resourceName: "screentime")),],
+       [Setting(name: "Основные", icon: #imageLiteral(resourceName: "main")),
+        Setting(name: "Пункт управления", icon: #imageLiteral(resourceName: "commandcenter")),
+        Setting(name: "Экран и яркость", icon: #imageLiteral(resourceName: "screen"))]
     ]
 }
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return settings.count
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return settings.count
+        return settings[section].count
         
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let setName = settings[indexPath.row]
+        let setName = settings[indexPath.section][indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! SettingsTableViewCell
         
         cell.nameOfSetting.text = setName.name
@@ -57,11 +62,6 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         cell.switch1.isHidden = setName.isSwitchHidden
         cell.accessoryType = setName.accessoryType1
    
-        if setName.icon == nil {
-            cell.backgroundColor = #colorLiteral(red: 0.9492684007, green: 0.9486895204, blue: 0.9704379439, alpha: 1)
-        } else {
-            cell.backgroundColor = #colorLiteral(red: 0.999904573, green: 1, blue: 0.9998808503, alpha: 1)
-        }
         
         return cell
     }
